b2e85b9b2051a32ec74ea851f87053ffe6e983c0
beto franca <beto2franca@gmail.com>
2023-07-05 20:03:16 -0300

    Repetições inicio

---
+++ aula013/ambiente.js
@@ -0,0 +1,27 @@
+var c = 1
+do{
+    console.log('opa!')
+   c++
+}while (c <= 4)
+
+
+
+
+
+/*var c = 1
+while (c < 6){
+    console.log('opa!')
+   c++
+}
+ */
+
+
+
+
+
+
+// console.log('tudo ok?')
+// console.log('tudo ok?')
+// console.log('tudo ok?')
+// console.log('tudo ok?')
+// console.log('tudo ok?')
