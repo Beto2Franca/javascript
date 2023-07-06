313c875623ee4ab131197432735cfe9b90dce5aa
beto franca <beto2franca@gmail.com>
2023-07-06 20:08:52 -0300

    inicio ex016

---
+++ aula014ex/ex016/script.js
@@ -0,0 +1,15 @@
+function calcular(){
+    var começo = document.querySelector('#inic')
+    var com = Number(começo.valeu)
+
+    var fim = document.querySelector('#ifim')
+    var final = Number(fim.value)
+
+    var pe = document.querySelector('#ipasso')
+    var passo = Number(pe.value)
+
+    
+
+
+
+}
