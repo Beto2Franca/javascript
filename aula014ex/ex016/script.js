abfe1d225afe5ce314dd87d758c871aebf22361b
beto franca <beto2franca@gmail.com>
2023-07-07 16:19:30 -0300

    Ex016 final

--- aula014ex/ex016/script.js
+++ aula014ex/ex016/script.js
@@ -1,14 +1,47 @@
 function calcular(){
+
     var começo = document.querySelector('#inic')
-    var com = Number(começo.valeu)
-
     var fim = document.querySelector('#ifim')
-    var final = Number(fim.value)
-
     var pe = document.querySelector('#ipasso')
-    var passo = Number(pe.value)
 
+    var res = document.querySelector('#res')
+
+    if (começo.value.length == 0 || fim.value.length == 0 || pe.value.length == 0){
+        res.innerHTML = 'Impossivel contar!'
+        // window.alert('[ERRO!] Faltam dados!')
+    } 
+    else{
+       res.innerHTML = 'Contando: <br>'
+
+       var i = Number(começo.value)
+       var f = Number(fim.value)
+       var p = Number(pe.value)
     
+       if (p<=0){
+        window.alert('Passo inválido! Considerando PASSO1')
+        p = 1
+       }
+
+       if(i<f){
+        // Contagem crescente 
+
+        for (var c = i ; c <= f; c = c + p){
+            res.innerHTML += ` ${c} \u{1f449} `
+       }
+
+       } else {
+        // Contagrem regressiva
+
+        for(var c = i; c>=f; c-=p){
+            res.innerHTML += `${c} \u{1F449}`
+        }
+       }
+
+       res.innerHTML += `\u{1F3C1}` 
+    //    A Bandeira no final
+    
+       
+    }
 
 
 
