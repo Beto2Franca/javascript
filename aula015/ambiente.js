fc52672c1ff7573d78bec291967ceff5844b853f
beto franca <beto2franca@gmail.com>
2023-08-07 18:30:34 -0300

    ayualização aula 016

--- aula015/ambiente.js
+++ aula015/ambiente.js
@@ -7,7 +7,7 @@ console.log(num)
 console.log(`Nosso Vetor tem ${num.length} posições`)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
 
-let pos = num.indexOf(4)
+let pos = num.indexOf(8)
 if(pos== -1){
     console.log(`O Valor não foi encontrado`)
 }else{
