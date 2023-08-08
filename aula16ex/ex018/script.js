9bc7d12160b08c5149a713596c8e6afc25a4f48c
beto franca <beto2franca@gmail.com>
2023-08-08 17:54:29 -0300

    aula 16 ex018

---
+++ aula16ex/ex018/script.js
@@ -0,0 +1,10 @@
+let num = document.querySelector('#fnum')
+let lista = document.querySelector('#flista')
+let res = document.querySelector('#res')
+
+let valores = []
+
+
+function adicionar(){
+
+}
