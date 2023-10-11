4b5d23c04b1e6a6a052d70abe74035ed6b176efa
beto franca <beto2franca@gmail.com>
2023-10-11 12:03:31 -0300

    exe final parte 01 JS

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -1,10 +1,31 @@
-let num = document.querySelector('#fnum')
-let lista = document.querySelector('#flista')
-let res = document.querySelector('#res')
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('selector#flista')
+let res = document.querySelector('div#res')
 
 let valores = []
 
 
-function adicionar(){
+function isNumero(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    }else{
+        return false
+    }
+}
 
+function inLista(n, l){
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    }else{
+        return false
+    }
+}
+
+
+function adicionar(){
+    if(isNumero(num.value) && !inLista(num.value, valores)){
+        valores.push(Number(num.value))
+    }else{
+        window.alert('Valor invalido ou já encontrado.')
+    }
 }
