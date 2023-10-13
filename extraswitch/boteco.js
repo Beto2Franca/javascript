1d0924299eddf9ef59b89b21d6e02322e83993f4
beto franca <beto2franca@gmail.com>
2023-10-13 10:46:05 -0300

    Update boteco.js

--- extraswitch/boteco.js
+++ extraswitch/boteco.js
@@ -14,35 +14,35 @@ function verificar() {
 
         switch (p) {
             case 1:
-                res.innerHTML = 'R$ 17,50'
+                res.innerHTML = ' 01 - R$ 17,50'
                 img.src = '/extraswitch/image/hvegan.png'
                 break
             case 2:
-                res.innerHTML = 'R$ 19,90'
+                res.innerHTML = '02 - R$ 19,90'
                 img.src = '/extraswitch/image/hcarne.png'
                 break
             case 3:
-                res.innerHTML = 'R$ 12,90'
+                res.innerHTML = '030 - R$ 12,90'
                 img.src = '/extraswitch/image/fritas.png'
                 break
             case 4:
-                res.innerHTML = 'R$ 16,90'
+                res.innerHTML = '04 - R$ 16,90'
                 img.src = '/extraswitch/image/coxinha.png'
                 break
             case 5:
-                res.innerHTML = 'R$ 15,90'
+                res.innerHTML = '05 - R$ 15,90'
                 img.src = '/extraswitch/image/caipirinha.png'
                 break
             case 6:
-                res.innerHTML = 'R$ 17,99'
+                res.innerHTML = '06 - R$ 17,99'
                 img.src = '/extraswitch/image/moscow.png'
                 break
             case 7:
-                res.innerHTML = 'R$ 13,99'
+                res.innerHTML = '07 - R$ 13,99'
                 img.src = '/extraswitch/image/cerva.png'
                 break
             case 8:
-                res.innerHTML = 'R$ 10,99'
+                res.innerHTML = '08 - R$ 10,99'
                 img.src = '/extraswitch/image/chopp.png'
                 break
             default:
@@ -59,7 +59,7 @@ function verificar() {
                 // Chama a função verificar() quando "Enter" é pressionado
                 verificar();
             }
-        });
+        })
 
 
 
